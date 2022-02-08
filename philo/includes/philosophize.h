17689d4ac51fc07c8165ceca8ce3b6176e1b5f6a
Jeremy Maia <jmaia@student.42.fr>
2022-02-08 15:48:14 +0100

    Finish main()

---
+++ philo/includes/philosophize.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophize.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 15:08:19 by jmaia             #+#    #+#             */
+/*   Updated: 2022/02/08 15:21:09 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHIZE_H
+# define PHILOSOPHIZE_H
+
+# include "philos_infos.h"
+
+int	philosophize(t_philos_infos pi);
+
+#endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,13 +6,42 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 12:47:04 by jmaia             #+#    #+#             */
-/*   Updated: 2022/02/08 14:46:30 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/08 15:20:38 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+#include  "it_is_not_libft_hoho.h"
+
 #include "parse_args.h"
+#include "philosophize.h"
+
+static void	print_syntax(const char *prog_name);
 
 int	main(int ac, char **av)
 {
-	parse_args(ac, av);
+	t_philos_infos	pi;
+	int				err;
+
+	pi = parse_args(ac, av);
+	if (pi.n_philos == -1)
+	{
+		print_syntax(av[0]);
+		return (1);
+	}
+	err = philosophize(pi);
+	if (err)
+	{
+		write(2, "An error occured while philosophizing\n", 38);
+		return (1);
+	}
+	return (0);
+}
+
+static void	print_syntax(const char *prog_name)
+{
+	write(2, prog_name, ft_strlen(prog_name));
+	write(2,	" <number_of_philosophers> <time_to_die> <time_to_eat>"
+				" <time_to_sleep> [number_of_meals]\n", 88);
 }
