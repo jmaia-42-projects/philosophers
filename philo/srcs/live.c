b5631a31f5acde0ee919d51131059a5f984e007a
Jeremy Maia <jmaia@student.42.fr>
2022-02-16 14:36:08 +0100

    Eat, think, and sleep

--- philo/srcs/live.c
+++ philo/srcs/live.c
@@ -6,16 +6,49 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 17:24:36 by jmaia             #+#    #+#             */
-/*   Updated: 2022/02/14 17:38:58 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/16 14:33:54 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "live.h"
 
+static void	parse_param(void *param, t_world **world, int *philo_id);
+static void	eat(int philo_id, t_world *world);
+static void	think(int philo_id, t_world *world);
+static void	sleep(int philo_id, t_world *world);
+
 void	*live(void *param)
 {
 	t_world	*world;
+	int		philo_id;
 
-	world = (t_world *)param;
+	parse_param(param, &world, &philo_id);
+	eat(philo_id, world);
+	think(philo_id, world);
+	sleep(philo_id, world);
 	return (0);
+}
+
+static void	parse_param(void *param, t_world **world, int *philo_id)
+{
+	void	**params;
+
+	params = (void **)param;
+	*world = (t_world *)params[0];
+	*philo_id = *(int *)params[1];
+}
+
+static void	eat(int philo_id, t_world *world)
+{
+
+}
+
+static void	think(int philo_id, t_world *world)
+{
+
+}
+
+static void sleep(int philo_id, t_world *world)
+{
+
 }
