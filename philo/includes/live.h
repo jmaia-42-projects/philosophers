2029ec185e20fb05ea01bd8ce2648cbe7be8eb40
Jeremy Maia <jmaia@student.42.fr>
2022-02-17 14:36:45 +0100

    Fix all errors and 'do something' in each actions

--- philo/includes/live.h
+++ philo/includes/live.h
@@ -6,13 +6,15 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 17:25:20 by jmaia             #+#    #+#             */
-/*   Updated: 2022/02/15 15:59:28 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/17 14:35:44 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIVE_H
 # define LIVE_H
 
+# include "actions.h"
+# include "do_something.h"
 # include "philos_infos.h"
 
 void	*live(void *param);
--- philo/srcs/live.c
+++ philo/srcs/live.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 17:24:36 by jmaia             #+#    #+#             */
-/*   Updated: 2022/02/16 15:22:56 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/02/17 14:35:03 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,15 @@ static void	parse_param(void *param, t_world **world, int *philo_id)
 
 static void	eat(int philo_id, t_world *world)
 {
-
+	do_something(philo_id, EAT, world);
 }
 
 static void	think(int philo_id, t_world *world)
 {
-	printf("%ld %d is thinking\n", world);
+	do_something(philo_id, THINK, world);
 }
 
 static void	sleep(int philo_id, t_world *world)
 {
-
+	do_something(philo_id, SLEEP, world);
 }
