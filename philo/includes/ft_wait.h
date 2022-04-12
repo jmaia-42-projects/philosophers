09abce7cfcfd04b9c97057fe7d1880e95f98f051
Jeremy Maia <jmaia@student.42.fr>
2022-04-12 12:58:02 +0200

    Use extern ref time instead of local

--- philo/includes/ft_wait.h
+++ philo/includes/ft_wait.h
@@ -6,14 +6,14 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 12:40:25 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/12 12:40:52 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/12 12:55:41 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_WAIT_H
 # define FT_WAIT_H
 
-void	ft_wait_ms(unsigned long wait_time_ms, struct timeval p_ref_time);
-void	ft_wait(struct timeval wait_time, struct timeval p_ref_time);
+void	ft_wait_ms(unsigned long wait_time_ms, struct timeval *ref_time);
+void	ft_wait(struct timeval wait_time, struct timeval *ref_time);
 
 #endif
