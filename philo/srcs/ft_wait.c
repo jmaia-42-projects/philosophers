ac45a23944ba54ba1e9fad53f14a5ef38cc00b77
Jeremy Maia <jmaia@student.42.fr>
2022-04-12 13:04:19 +0200

    Revert "Use extern ref time instead of local"
    This reverts commit 09abce7cfcfd04b9c97057fe7d1880e95f98f051.

--- philo/srcs/ft_wait.c
+++ philo/srcs/ft_wait.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 12:13:41 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/12 12:56:02 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/12 12:42:57 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,29 @@ static int				is_greater_than(struct timeval a, struct timeval b);
 static struct timeval	time_diff(struct timeval a, struct timeval b);
 static struct timeval	time_sum(struct timeval a, struct timeval b);
 
-void	ft_wait_ms(unsigned long wait_time_ms, struct timeval *ref_time)
+void	ft_wait_ms(unsigned long wait_time_ms, struct timeval p_ref_time)
 {
 	struct timeval	wait_time;
 
 	wait_time.tv_sec = wait_time_ms / 1000;
 	wait_time.tv_usec = wait_time_ms % 1000 * 1000;
-	ft_wait(wait_time, ref_time);
+	ft_wait(wait_time, p_ref_time);
 }
 
-void	ft_wait(struct timeval wait_time, struct timeval *ref_time)
+void	ft_wait(struct timeval wait_time, struct timeval p_ref_time)
 {
+	static struct timeval	ref_time;
 	struct timeval			now;
 
+	if (p_ref_time.tv_sec)
+		ref_time = p_ref_time;
 	gettimeofday(&now, 0);
-	while (is_greater_than(time_diff(now, *ref_time), wait_time))
+	while (is_greater_than(time_diff(now, ref_time), wait_time))
 	{
 		usleep(1);
 		gettimeofday(&now, 0);
 	}
-	*ref_time = time_sum(*ref_time, wait_time);
+	ref_time = time_sum(ref_time, wait_time);
 }
 
 static int	is_greater_than(struct timeval a, struct timeval b)
