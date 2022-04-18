36fca1564838e57ba82702e8de4ba2c57ca0a997
Jeremy Maia <jmaia@student.42.fr>
2022-04-18 20:00:48 +0200

    Fix mutex type in philo structure

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,19 +6,21 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 10:10:03 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/16 10:16:22 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/18 19:21:21 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
+
 typedef struct s_philo
 {
 	int					id;
 	int					last_eat;
-	pthread_mutexattr_t	left_fork;
-	phtread_mutexattr_t	right_fork;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
 }	t_philo;
 
 #endif
