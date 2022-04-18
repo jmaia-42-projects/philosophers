1a550cb3b8e58b8fae3b96d483576032510d1aeb
Jeremy Maia <jmaia@student.42.fr>
2022-04-18 21:01:42 +0200

    Add init_forks

---
+++ philo/srcs/init_forks.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_forks.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 20:38:00 by jmaia             #+#    #+#             */
+/*   Updated: 2022/04/18 21:01:22 by jmaia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "simulation.h"
+
+#include <stdlib.h>
+
+static int				give_fork_to_philos(t_philo *left_philo,
+							t_philo *right_philo);
+static pthread_mutex_t	*init_fork(void);
+
+int	init_forks(t_philo *philos, t_philos_infos *pi)
+{
+	int				err;
+	int				i;
+	t_philo			*left_philo;
+
+	i = 0;
+	err = 0;
+	left_philo = 0;
+	while (i < pi->n_philos && !err)
+	{
+		if (i == 0 && pi->n_philos > 1)
+			left_philo = &philos[pi->n_philos - 1];
+		else if (pi->n_philos > 1)
+			left_philo = &philos[i - 1];
+		err = give_fork_to_philos(left_philo, &philos[i]);
+	}
+	// USE YOUR MIND
+	return (0);
+}
+
+static int	give_fork_to_philos(t_philo *left_philo, t_philo *right_philo)
+{
+	right_philo->left_fork = init_fork();
+	if (!right_philo->left_fork)
+		return (1);
+	if (left_philo)
+		left_philo->right_fork = right_philo->left_fork;
+	return (0);
+}
+
+static pthread_mutex_t	*init_fork(void)
+{
+	int				err;
+	pthread_mutex_t	*fork;
+
+	fork = malloc(sizeof(*fork));
+	if (!fork)
+		return (0);
+	err = pthread_mutex_init(fork, 0);
+	if (err)
+	{
+		free(fork);
+		return (0);
+	}
+	return (fork);
+}
