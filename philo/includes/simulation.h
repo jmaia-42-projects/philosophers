9aadfbab3501beed39e05fe3f41e2d89a7d9af0f
Jeremy Maia <jmaia@student.42.fr>
2022-04-18 20:37:45 +0200

    Create init_philos

--- philo/includes/simulation.h
+++ philo/includes/simulation.h
@@ -6,12 +6,15 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 19:38:31 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/18 19:58:26 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/18 20:36:27 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIMULATION_H
 # define SIMULATION_H
+
+# include "philo.h"
+# include "philos_infos.h"
 
 int		init_forks(t_philo *philos, t_philos_infos *pi);
 int		init_philos(t_philo **philos, t_philos_infos *pi);
