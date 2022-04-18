b15ad363c073e9c685b3920ead5ecebfc00ae445
Jeremy Maia <jmaia@student.42.fr>
2022-04-18 20:01:20 +0200

    Add parse_with_error in header file

--- philo/includes/parse_args.h
+++ philo/includes/parse_args.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 14:39:16 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/16 09:59:42 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/16 10:38:39 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 # include "philos_infos.h"
 
-void	parse_args(t_philos_infos *pi, int ac, char **av);
+int	parse_args_and_print_error(t_philos_infos **pi, int ac, char **av);
+int	parse_args(t_philos_infos **pi, int ac, char **av);
 
 #endif
