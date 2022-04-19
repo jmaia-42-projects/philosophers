9264f74a9e51746f3ef657f35803b16558908770
Jeremy Maia <jmaia@student.42.fr>
2022-04-19 12:56:01 +0200

    Fill ph_sleep

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 10:10:03 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/18 20:24:25 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/19 12:53:36 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <pthread.h>
 
+# include "philos_infos.h"
+
 typedef struct s_philo
 {
 	int					id;
@@ -22,6 +24,7 @@ typedef struct s_philo
 	pthread_mutex_t		*left_fork;
 	pthread_mutex_t		*right_fork;
 	pthread_t			thread;
+	t_philos_infos		*pi;
 }	t_philo;
 
 #endif
--- philo/srcs/actions/ph_sleep.c
+++ philo/srcs/actions/ph_sleep.c
@@ -6,14 +6,18 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 12:13:32 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/19 12:18:39 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/19 12:55:51 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "actions/actions.h"
 
+#include "actions/do_actions.h"
+
 int	ph_sleep(t_philo *philo)
 {
-	(void) philo;
-	return (0);
+	int	err;
+
+	err = do_action(philo->pi->time_to_sleep, philo->id, SLEEP_MSG);
+	return (err);
 }
