93ad611020defbd5b568d4f388cbdc99587c9082
Jeremy Maia <jmaia@student.42.fr>
2022-04-19 13:01:38 +0200

    Fill ph_think()

--- philo/srcs/actions/ph_think.c
+++ philo/srcs/actions/ph_think.c
@@ -6,14 +6,18 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 12:13:32 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/19 12:18:44 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/19 13:01:19 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "actions/actions.h"
 
+#include "actions/do_actions.h"
+
 int	ph_think(t_philo *philo)
 {
-	(void) philo;
-	return (0);
+	int	err;
+
+	err = do_action(0, philo->id, THINK_MSG);
+	return (err);
 }
