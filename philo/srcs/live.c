11d7a59e5f1f2c6e8b29b5b8ab05fc7ab019357f
Jeremy Maia <jmaia@student.42.fr>
2022-04-20 10:11:34 +0200

    Create is_simulation_over()

--- philo/srcs/live.c
+++ philo/srcs/live.c
@@ -6,11 +6,13 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 11:30:28 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/19 12:19:08 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/20 10:12:02 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "simulation.h"
+
+static int	is_simulation_over(t_philo *philo);
 
 void	*live(void *param)
 {
@@ -26,4 +28,16 @@ void	*live(void *param)
 		ph_think(philo);
 	}
 	return (0);
+}
+
+static int	is_simulation_over(t_philo *philo)
+{
+	int	is_simulation_over;
+
+	if (philo->n_meals >= philo->state->pi.n_meals)
+		return (1);
+	pthread_mutex_lock(&philo->state->is_simulation_over_lock);
+	is_simulation_over = philo->state->is_simulation_over;
+	pthread_mutex_unlock(&philo->state->is_simulation_over_lock);
+	return (is_simulation_over);
 }
