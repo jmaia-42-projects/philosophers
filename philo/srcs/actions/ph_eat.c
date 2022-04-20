988e30c02878868e020ce446396f2f301101f419
Jeremy Maia <jmaia@student.42.fr>
2022-04-20 11:05:04 +0200

    Fill ph_eat()

--- philo/srcs/actions/ph_eat.c
+++ philo/srcs/actions/ph_eat.c
@@ -6,15 +6,23 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 12:13:32 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/20 10:15:11 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/20 11:04:23 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "actions/actions.h"
 
+#include "actions/do_actions.h"
+
 int	ph_eat(t_philo *philo)
 {
-	(void) philo;
+	int	err;
+
+	ph_take_fork(philo, philo->left_fork);
+	ph_take_fork(philo, philo->right_fork);
+	err = do_action(philo, philo->state->pi.time_to_eat, EAT_MSG);
+	ph_release_fork(philo->left_fork);
+	ph_release_fork(philo->right_fork);
 	philo->n_meals++;
-	return (0);
+	return (err);
 }
