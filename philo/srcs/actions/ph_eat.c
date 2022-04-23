09aa27a926141f75b8abae0ba28fe51280e4a71f
Jeremy Maia <jmaia@student.42.fr>
2022-04-23 17:59:10 +0200

    Set last_eat variable

--- philo/srcs/actions/ph_eat.c
+++ philo/srcs/actions/ph_eat.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 12:13:32 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/23 11:40:54 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/23 17:58:46 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ph_eat(t_philo *philo)
 
 	ph_take_fork(philo, philo->left_fork);
 	ph_take_fork(philo, philo->right_fork);
+	philo->last_eat = philo->timestamp;
 	err = do_action(philo, philo->state->pi.time_to_eat, EAT_MSG);
 	ph_release_fork(philo->left_fork);
 	ph_release_fork(philo->right_fork);
