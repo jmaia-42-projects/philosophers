ebfcfe78083d93f73f039aec5ef8991fb2421991
Jeremy Maia <jmaia@student.42.fr>
2022-04-24 23:40:12 +0200

    OKEYYYY BOY I FIXED THAT TOO <3

--- philo/srcs/init_philos.c
+++ philo/srcs/init_philos.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 20:26:54 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/20 10:48:48 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/24 23:39:49 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	init_philos(t_philo **philos, t_simulation_state *state)
 static void	init_philo(t_philo *philo, int id, t_simulation_state *state)
 {
 	philo->id = id;
-	philo->last_eat = 0;
+	philo->last_eat = -1;
 	philo->left_fork = 0;
 	philo->right_fork = 0;
 	philo->n_meals = 0;
--- philo/srcs/live.c
+++ philo/srcs/live.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 11:30:28 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/23 23:23:11 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/24 23:39:30 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 static int	is_simulation_over(t_philo *philo);
 static void	delay_start(t_philo *philo);
+static void	wait_till_next_meal(t_philo *philo);
 
 void	*live(void *param)
 {
@@ -25,6 +26,7 @@ void	*live(void *param)
 	delay_start(philo);
 	while (!is_simulation_over(philo))
 	{
+		wait_till_next_meal(philo);
 		ph_eat(philo);
 		ph_sleep(philo);
 		ph_think(philo);
@@ -52,4 +54,29 @@ static int	is_simulation_over(t_philo *philo)
 	is_simulation_over = philo->state->is_simulation_over;
 	pthread_mutex_unlock(&philo->state->is_simulation_over_lock);
 	return (is_simulation_over);
+}
+
+static void	wait_till_next_meal(t_philo *philo)
+{
+	unsigned long	next_meal;
+
+	if (philo->state->pi.n_philos % 2 != 1)
+	{
+		if (philo->last_eat == (unsigned long) -1)
+			philo->last_eat = 0;
+		return ;
+	}
+	if (philo->last_eat == (unsigned long) -1)
+	{
+		if (philo->id == philo->state->pi.n_philos)
+			next_meal = 0;
+		else if (philo->id % 2 == 0)
+			next_meal = philo->state->pi.time_to_eat;
+		else
+			next_meal = philo->state->pi.time_to_eat * 2;
+	}
+	else
+		next_meal = philo->last_eat + 3 * philo->state->pi.time_to_eat;
+	ft_wait_ms_until(next_meal, 0);
+	philo->timestamp = next_meal;
 }
